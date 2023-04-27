0904f856aa727356b9a9ae0da269ec855439968a
Trevor <galentrevor@gmail.com>
2023-04-27 11:01:36 -0400

    Login and registration working and added new html templates

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from sqlalchemy import Column, Integer, String, Numeric, create_engine, text
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'BigSecret'
-
 conn_str = "mysql://root:Treyjg2121@localhost/ecommerce"
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
@@ -14,7 +12,24 @@ def index():
 
 @app.route("/Login", methods=['GET','POST'])
 def Login():
-    return render_template("Login.html")
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        account_type = request.form['account_type']
+        user_query = text("select * from accounts where username = :username AND password = :password AND account_type = :account_type" )
+        params = {"username": username, "password": password, "account_type" :account_type}
+        with engine.connect() as conn:
+            user = conn.execute(user_query, params).fetchone()
+        if user is None:
+            return render_template('Login.html')
+        if account_type == "Admin":
+            return render_template('Accounts.html')
+        if account_type == "Vendor":
+            return render_template('VendorAdd.html')
+        if account_type == "User":
+            return render_template("Products.html")
+    else:
+        return render_template("Login.html")
 
 @app.route("/Registration", methods=['GET','POST'])
 def Registration():
@@ -37,6 +52,14 @@ def Registration():
 @app.route('/Products', methods=['GET','POST'])
 def Products():
     return render_template('Products.html')
+
+@app.route('/Accounts', methods=['GET','POST'])
+def Accounts():
+    return render_template('Accounts.html')
+
+@app.route('/VendorAdd', methods=['GET','POST'])
+def AddProducts():
+    return render_template('VendorAdd.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
