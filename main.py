8db1e78dec92be34f063048555811e5505bee5ac
Trevor <galentrevor@gmail.com>
2023-05-10 11:01:11 -0400

    Chat Page errors fixed, Added Design to Chat, Product Page allows access after login

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for, session,flash
+from flask import Flask, render_template, request, redirect, url_for, session,flash,abort
 from sqlalchemy import Column, Integer, String, Numeric, create_engine, text
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'BigSecret21'
 conn_str = "mysql://root:Treyjg2121@localhost/ecommerce"
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
+
+def t(str):
+    alph = "abcdefghijklmnopqrstuvwxyz"
+    sum = 0
+    for char in str:
+        sum += alph.find(char) + 1
+    return sum
 
 @app.route("/", methods=['GET','POST'])
 def index():
@@ -70,17 +77,29 @@ def Products():
             products = conn.execute(query).fetchall()
         return render_template('Products.html', products=products)
 
-@app.route('/cart', methods=['POST'])
+@app.route('/cart', methods=['GET', 'POST'])
 def cart():
+    if request.method == 'POST':
+        product_id = request.form.get('product_id')
+        if product_id:
+            if 'cart' not in session:
+                session['cart'] = {}
+            if product_id in session['cart']:
+                session['cart'][product_id] += 1
+            else:
+                session['cart'][product_id] = 1
     cart_items = []
+    cart_total = 0
     for product_id, quantity in session.get('cart', {}).items():
-        query = text("SELECT * FROM products WHERE product_id = :product_id")
-        params = {'product_id': product_id}
-        with engine.connect() as conn:
-            product = conn.execute(query, params).fetchone()
-        if product is not None:
-            cart_items.append((product, quantity))
-    return render_template('cart.html', cart_items=cart_items)
+            query = text("SELECT * FROM products WHERE product_id = :product_id")
+            params = {'product_id': product_id}
+            with engine.connect() as conn:
+                product = conn.execute(query, params).fetchone()
+            if product is not None:
+                cart_items.append((product, quantity))
+                cart_total += product.product_cost * quantity
+            return render_template('cart.html', cart_items=cart_items, cart_total=cart_total)
+
 
 @app.route('/Accounts', methods=['GET','POST'])
 def Accounts():
@@ -94,25 +113,32 @@ def Accounts():
     else:
         return redirect(url_for('Login'))
 
-@app.route('/VendorAdd', methods=['GET','POST'])
+@app.route('/VendorAdd', methods=['GET', 'POST'])
 def AddProducts():
     if request.method == 'POST':
-        product_name = request.form['product_name']
-        product_type = request.form['product_type']
-        product_cost = request.form['product_cost']
-        vendor_name = request.form['vendor_name']
-        img_url = request.form['img_url']
-        query = text(
-            "INSERT INTO products(product_name,product_type,product_cost,vendor_name,img_url)"
-            "VALUES(:product_name, :product_type, :product_cost, :vendor_name, :img_url)")
-        params = {"product_name": product_name, "product_type": product_type, "product_cost": product_cost, "vendor_name": vendor_name, "img_url": img_url}
-        with engine.connect() as conn:
-            conn.execute(query, params)
-            conn.commit()
-            return render_template("Products.html")
+        if session['account_type'] == "Admin":
+            product_name = request.form['product_name']
+            product_type = request.form['product_type']
+            product_cost = request.form['product_cost']
+            vendor_name = request.form['vendor_name']
+            img_url = request.form['img_url']
+            query = text("INSERT INTO products(product_name, product_type, product_cost, vendor_name, img_url) "
+                         "VALUES(:product_name, :product_type, :product_cost, :vendor_name, :img_url)")
+            params = {"product_name": product_name, "product_type": product_type, "product_cost": product_cost,
+                      "vendor_name": vendor_name, "img_url": img_url}
+            with engine.connect() as conn:
+                conn.execute(query, params)
+                conn.commit()
+                return render_template("Products.html")
+        elif session['account_type'] == "Vendor":
+            return render_template("VendorAdd.html")
+        else:
+            return abort(403)
     else:
-        return render_template("VendorAdd.html")
-
+        if session['account_type'] == "Vendor" or session['account_type'] == "Admin":
+            return render_template("VendorAdd.html")
+        else:
+            return abort(403)
 @app.route('/Chat', methods=['GET', 'POST'])
 def Chat():
     if request.method == 'POST':
